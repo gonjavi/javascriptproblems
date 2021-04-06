770358ea4f1dbde9c90af1561852d2b2355aa6f2
manci <javiercaliescali@gmail.com>
2021-04-05 19:42:39 -0500

    -

---
+++ pasarUsuarioPremium.js
@@ -0,0 +1,30 @@
+
+
+function pasarUsuarioAPremium(usuarios) {
+  // "usuarios" es un array de objetos "usuario"
+  // Cada objeto "usuario" tiene la propiedad "esPremium"
+  // Define cada propiedad "esPremium" de cada objeto como "true"
+  // Devuelve el array de usuarios
+  // Tu código:
+  for (var i in usuarios) {
+    usuarios[i].esPremium = true;
+  }
+
+  return usuarios; 
+}
+
+console.log(pasarUsuarioAPremium([
+  {
+    "esPremium": false
+  },
+  {
+    "esPremium": false
+  },
+  {
+    "esPremium": false
+  },
+  {
+    "esPremium": false
+  }
+]));
+
